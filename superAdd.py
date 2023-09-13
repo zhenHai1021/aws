9ffd69375ba0d2271f7d0a6eeed3ba6f95173e78
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-09-14 01:25:09 +0800

    Update superAdd.py

--- superAdd.py
+++ superAdd.py
@@ -34,7 +34,7 @@ def allowed_file(filename):
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddSupervisor.html')
+    return render_template('StudentApproval.html')
 
 @app.route("/addsupervisor", methods=['POST'])
 def AddSupervisor():
